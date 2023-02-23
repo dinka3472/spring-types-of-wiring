9c748470df477daf4669e475351fd38b9653fe8e
Dinara Gafiiatullina <irishdance.ufa@gmail.com>
2023-02-24 03:53:01 +0500

    bugs fixed1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle;
+    private  Needle7 needle;
 
-    @Autowired
     public Egg6(Needle7 needle) {
         this.needle = needle;
     }
